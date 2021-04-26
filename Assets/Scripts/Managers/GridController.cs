3a9038e3c09fd0fdd8420f277abfb5a85ed18d6d
Jack Coggins <voidawake90@gmail.com>
2021-04-26 20:17:04 +1000

    Move functions from GridController to GridManager

--- Assets/Scripts/Managers/GridController.cs
+++ Assets/Scripts/Managers/GridController.cs
@@ -1,71 +1,37 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
-using TileData = Tiles.TileData;
 
 namespace Managers
 {
     public class GridController : MonoBehaviour
     {
-        [SerializeField] private Tilemap tilemap;
-
-        [SerializeField] private List<TileData> tileDatas = new List<TileData>();
-
-        private Grid grid { get; set; }
+        private Tilemap tilemap;
+        private GridManager gridManager;
 
         private void Awake()
         {
-            grid = GetComponent<Grid>();
-            ManagerLocator.Get<GridManager>().Controller = this;
             tilemap = GetComponentInChildren<Tilemap>();
             
-            BoundsInt bounds = tilemap.cellBounds;
-            TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
-            
-            for (int x = 0; x < bounds.size.x; x++)
-            {
-                for (int y = 0; y < bounds.size.y; y++)
-                {
-                    TileBase tile = allTiles[x + y * bounds.size.x];
-
-                    if (tile != null)
-                    {
-                        TileData tileData = new TileData(tile, new Vector2(x, y));
-                        tileDatas.Add(tileData);
-                        //Debug.Log(tileData.Tile.name + " is at position " + tileData.Position);
-                    }
-                }
-            }
+            gridManager = ManagerLocator.Get<GridManager>();
+            gridManager.Controller = this;
+            gridManager.Initialise(tilemap);
             
             TestingGetGridObjectsByCoordinate(10);
-            
-        }
-        
-        public TileBase GetGridObjectsByCoordinate(float x, float z)
-        {
-            Vector2 coordinate = new Vector2(x, z);
-            foreach (TileData tileData in tileDatas)
-            {
-                if (tileData.Position == coordinate)
-                {
-                    return tileData.Tile;
-                }
-            }
-
-            Debug.LogError("ERROR: No tile was found for the provided coordinates " + coordinate);
-            return null;
         }
 
         #region Function Testing
         
-        private void TestingGetGridObjectsByCoordinate(float successfulCases)
+        private void TestingGetGridObjectsByCoordinate(int testCases)
         {
-            for (int i = 0; i < successfulCases; i++)
+            for (int i = 0; i < testCases; i++)
             {
                 Vector2 randomCoordinates = new Vector2(Random.Range(2,18), Random.Range(0,12));
-                print(GetGridObjectsByCoordinate(randomCoordinates.x, randomCoordinates.y) + " is at the provided coordinates " + randomCoordinates);
+                TileBase tile = gridManager.GetGridObjectsByCoordinate(
+                    randomCoordinates.x,
+                    randomCoordinates.y
+                );
+                print(tile + " is at the provided coordinates " + randomCoordinates);
             }
             
             // print(GetGridObjectsByCoordinate(1, -1) + "Error should return since coordinates are out of bounds");
--- Assets/Scripts/Managers/GridManager.cs
+++ Assets/Scripts/Managers/GridManager.cs
@@ -1,7 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using TileData = Tiles.TileData;
+
 namespace Managers
 {
     public class GridManager : IManager
     {
         public GridController Controller { get; set; }
+        
+        private List<TileData> tileDatas = new List<TileData>();
+
+        public void Initialise(Tilemap tilemap)
+        {
+            BoundsInt bounds = tilemap.cellBounds;
+            TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+            
+            for (int x = 0; x < bounds.size.x; x++)
+            {
+                for (int y = 0; y < bounds.size.y; y++)
+                {
+                    TileBase tile = allTiles[x + y * bounds.size.x];
+
+                    if (tile != null)
+                    {
+                        TileData tileData = new TileData(tile, new Vector2(x, y));
+                        tileDatas.Add(tileData);
+                        //Debug.Log(tileData.Tile.name + " is at position " + tileData.Position);
+                    }
+                }
+            }
+        }
+        
+        public TileBase GetGridObjectsByCoordinate(float x, float z)
+        {
+            Vector2 coordinate = new Vector2(x, z);
+            foreach (TileData tileData in tileDatas)
+            {
+                if (tileData.Position == coordinate)
+                {
+                    return tileData.Tile;
+                }
+            }
+
+            Debug.LogError("ERROR: No tile was found for the provided coordinates " + coordinate);
+            return null;
+        }
     }
 }
