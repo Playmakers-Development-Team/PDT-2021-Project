75d90f2ddc37c907b890f10e504ae05b8d97dcc4
Jordan <Jordan.Hamlin6@gmail.com>
2021-04-26 20:04:27 +1000

    Tweaked base classes.

--- Assets/Scripts/Background/BackgroundCamera.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-namespace Background
-{
-    public class BackgroundCamera : MonoBehaviour
-    {
-    }
-}
---
+++ Assets/Scripts/Background/Camera.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Background
+{
+    [RequireComponent(typeof(UnityEngine.Camera))]
+    public class Camera : MonoBehaviour
+    {
+        [SerializeField] private Vector2Int resolution;
+        
+        [Space]
+        
+        [SerializeField] private LayerMask washLayer;
+        [SerializeField] private Renderer washRenderer;
+        [SerializeField] private string washTexturePropertyName;
+        
+        [Space]
+        
+        [SerializeField] private LayerMask lineLayer;
+        [SerializeField] private Renderer lineRenderer;
+        [SerializeField] private string lineTexturePropertyName;
+        
+        private new UnityEngine.Camera camera;
+        private RenderTexture washTexture;
+        private RenderTexture lineTexture;
+        
+
+        private void Awake()
+        {
+            TryGetComponent(out camera);
+        }
+
+        public void Render()
+        {
+            
+        }
+    }
+}
