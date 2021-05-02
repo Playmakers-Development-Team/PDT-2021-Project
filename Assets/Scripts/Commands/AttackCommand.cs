53207af475e0cfae59aaf9a3b9d1ab36b53a7cbf
Jack Coggins <voidawake90@gmail.com>
2021-05-02 17:48:00 +1000

    Add AttackCommand class

--- Assets/Scripts/Commands/AttackCommand.cs
+++ Assets/Scripts/Commands/AttackCommand.cs
@@ -1,15 +1,33 @@
+using GridObjects;
 using Units;
 
 namespace Commands
 {
     public class AttackCommand : Command
     {
-        public AttackCommand(IUnit unit) : base(unit) {}
+        private GridObject target;
+        private int damage;
+        private int knockback;
+
+        public AttackCommand(IUnit unit, GridObject target, int damage, int knockback) : base(unit)
+        {
+            this.target = target;
+            this.damage = damage;
+            this.knockback = knockback;
+        }
 
         public override void Queue() {}
 
-        public override void Execute() {}
+        public override void Execute()
+        {
+            target.TakeDamage(damage);
+            target.TakeKnockback(knockback);
+        }
 
-        public override void Undo() {}
+        public override void Undo()
+        {
+            target.TakeDamage(-damage);
+            target.TakeKnockback(-knockback);
+        }
     }
 }
