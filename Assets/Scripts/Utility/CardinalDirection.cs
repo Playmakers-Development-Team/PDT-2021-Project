d5cce1838177fe95e46ae9bbdf321f0bed67466a
A-G-K <agk8097@gmail.com>
2021-05-25 16:18:42 +1000

    Rename QuadDirection to CardinalDirection and Direction to OrdinalDirection

---
+++ Assets/Scripts/Utility/CardinalDirection.cs
@@ -0,0 +1,213 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Utility
+{
+    public enum CardinalDirection
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    public static class CardinalDirectionExtensions
+    {
+        public static CardinalDirection Opposite(this CardinalDirection direction) =>
+            (CardinalDirection) (((int) direction + 2) % 4);
+
+        public static CardinalDirection RotateClockwise(this CardinalDirection direction) =>
+            (CardinalDirection) (((int) direction + 1) % 4);
+
+        public static CardinalDirection RotateAntiClockwise(this CardinalDirection direction)
+        {
+            int directionIndex = (int) direction - 1;
+            return (CardinalDirection) (directionIndex < 0 ? 3 : directionIndex % 4);
+        }
+
+        public static Vector2Int ToVector2Int(this CardinalDirection direction)
+        {
+            switch (direction)
+            {
+                case CardinalDirection.East:
+                    return Vector2Int.right;
+                case CardinalDirection.South:
+                    return Vector2Int.down;
+                case CardinalDirection.West:
+                    return Vector2Int.left;
+                default:
+                    return Vector2Int.up;
+            }
+        }
+
+        public static Vector2 ToVector2(this CardinalDirection direction)
+        {
+            switch (direction)
+            {
+                case CardinalDirection.East:
+                    return Vector2.right;
+                case CardinalDirection.South:
+                    return Vector2.down;
+                case CardinalDirection.West:
+                    return Vector2.left;
+                default:
+                    return Vector2.up;
+            }
+        }
+
+        public static CardinalDirectionMask ToMask(this CardinalDirection direction)
+        {
+            switch (direction)
+            {
+                case CardinalDirection.East:
+                    return CardinalDirectionMask.East;
+                case CardinalDirection.South:
+                    return CardinalDirectionMask.South;
+                case CardinalDirection.West:
+                    return CardinalDirectionMask.West;
+                default:
+                    return CardinalDirectionMask.North;
+            }
+        }
+
+        public static OrdinalDirection ToDirection(this CardinalDirection direction)
+        {
+            switch (direction)
+            {
+                case CardinalDirection.East:
+                    return OrdinalDirection.East;
+                case CardinalDirection.South:
+                    return OrdinalDirection.South;
+                case CardinalDirection.West:
+                    return OrdinalDirection.West;
+                default:
+                    return OrdinalDirection.North;
+            }
+        }
+
+        public static bool IsVertical(this CardinalDirection direction) =>
+            direction == CardinalDirection.North || direction == CardinalDirection.South;
+
+        public static bool IsHorizontal(this CardinalDirection direction) =>
+            direction == CardinalDirection.East || direction == CardinalDirection.West;
+    }
+
+    [Flags]
+    public enum CardinalDirectionMask
+    {
+        None = 0,
+        North = 1,
+        East = 2,
+        South = 4,
+        West = 8,
+    }
+
+    public static class CardinalDirectionMaskExtensions
+    {
+        public static bool Contains(this CardinalDirectionMask mask, CardinalDirection direction) =>
+            (mask & direction.ToMask()) != 0;
+
+        public static IEnumerable<CardinalDirectionMask> Split(this CardinalDirectionMask mask)
+        {
+            List<CardinalDirectionMask> enumerable = new List<CardinalDirectionMask>(4);
+
+            if ((mask & CardinalDirectionMask.North) != 0)
+            {
+                enumerable.Add(CardinalDirectionMask.North);
+            }
+
+            if ((mask & CardinalDirectionMask.East) != 0)
+            {
+                enumerable.Add(CardinalDirectionMask.East);
+            }
+
+            if ((mask & CardinalDirectionMask.West) != 0)
+            {
+                enumerable.Add(CardinalDirectionMask.West);
+            }
+
+            if ((mask & CardinalDirectionMask.South) != 0)
+            {
+                enumerable.Add(CardinalDirectionMask.South);
+            }
+
+            return enumerable;
+        }
+
+        public static IEnumerable<CardinalDirection> GetDirections(this CardinalDirectionMask mask)
+        {
+            List<CardinalDirection> enumerable = new List<CardinalDirection>(4);
+
+            if ((mask & CardinalDirectionMask.North) != 0)
+            {
+                enumerable.Add(CardinalDirection.North);
+            }
+
+            if ((mask & CardinalDirectionMask.East) != 0)
+            {
+                enumerable.Add(CardinalDirection.East);
+            }
+
+            if ((mask & CardinalDirectionMask.West) != 0)
+            {
+                enumerable.Add(CardinalDirection.West);
+            }
+
+            if ((mask & CardinalDirectionMask.South) != 0)
+            {
+                enumerable.Add(CardinalDirection.South);
+            }
+
+            return enumerable;
+        }
+
+        public static bool HasVertical(this CardinalDirectionMask mask) =>
+            (mask & CardinalDirectionMask.North) != 0 || (mask & CardinalDirectionMask.South) != 0;
+
+        public static bool HasHorizontal(this CardinalDirectionMask mask) =>
+            (mask & CardinalDirectionMask.East) != 0 || (mask & CardinalDirectionMask.West) != 0;
+    }
+
+    public static class CardinalDirectionUtil
+    {
+        public static CardinalDirection From(Vector2 fromPos, Vector2 toPos)
+        {
+            Vector2 vector = (toPos - fromPos).normalized;
+            float angle = Vector2.SignedAngle(Vector2.up, vector);
+
+            if (angle >= 45 && angle < 135)
+            {
+                return CardinalDirection.West;
+            }
+
+            if (angle < -45 && angle >= -135)
+            {
+                return CardinalDirection.East;
+            }
+
+            if (angle >= -45 && angle < 45)
+            {
+                return CardinalDirection.North;
+            }
+
+            return CardinalDirection.South;
+        }
+
+        public static CardinalDirectionMask GetMaskFrom(params CardinalDirection[] directions) =>
+            GetMaskFrom(directions.AsEnumerable());
+
+        public static CardinalDirectionMask GetMaskFrom(IEnumerable<CardinalDirection> directions)
+        {
+            CardinalDirectionMask mask = CardinalDirectionMask.None;
+
+            foreach (CardinalDirection direction in directions)
+            {
+                mask |= direction.ToMask();
+            }
+
+            return mask;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/OrdinalDirection.cs
@@ -0,0 +1,328 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Utility
+{
+    public enum OrdinalDirection
+    {
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest
+    }
+
+    public static class OrdinalDirectionExtensions
+    {
+        public static OrdinalDirection Opposite(this OrdinalDirection direction) =>
+            (OrdinalDirection) (((int) direction + 4) % 8);
+
+        public static OrdinalDirection RotateClockwise(this OrdinalDirection direction) =>
+            (OrdinalDirection) (((int) direction + 1) % 8);
+
+        public static OrdinalDirection RotateAntiClockwise(this OrdinalDirection direction)
+        {
+            int directionIndex = (int) direction - 1;
+            return (OrdinalDirection) (directionIndex < 0 ? 7 : directionIndex % 8);
+        }
+
+        public static Vector2Int ToVector2Int(this OrdinalDirection direction)
+        {
+            switch (direction)
+            {
+                case OrdinalDirection.North:
+                    return Vector2Int.up;
+                case OrdinalDirection.NorthEast:
+                    return Vector2Int.up + Vector2Int.right;
+                case OrdinalDirection.East:
+                    return Vector2Int.right;
+                case OrdinalDirection.SouthEast:
+                    return Vector2Int.down + Vector2Int.right;
+                case OrdinalDirection.South:
+                    return Vector2Int.down;
+                case OrdinalDirection.SouthWest:
+                    return Vector2Int.down + Vector2Int.left;
+                case OrdinalDirection.West:
+                    return Vector2Int.left;
+                case OrdinalDirection.NorthWest:
+                    return Vector2Int.up + Vector2Int.left;
+            }
+
+            throw new Exception($"{nameof(OrdinalDirection)} {direction} is not handled");
+        }
+
+        public static Vector2 ToVector2(this OrdinalDirection direction)
+        {
+            switch (direction)
+            {
+                case OrdinalDirection.North:
+                    return Vector2.up;
+                case OrdinalDirection.NorthEast:
+                    return Vector2.up + Vector2.right;
+                case OrdinalDirection.East:
+                    return Vector2.right;
+                case OrdinalDirection.SouthEast:
+                    return Vector2.down + Vector2.right;
+                case OrdinalDirection.South:
+                    return Vector2.down;
+                case OrdinalDirection.SouthWest:
+                    return Vector2.down + Vector2.left;
+                case OrdinalDirection.West:
+                    return Vector2.left;
+                case OrdinalDirection.NorthWest:
+                    return Vector2.up + Vector2.left;
+            }
+
+            throw new Exception($"{nameof(OrdinalDirection)} {direction} is not handled");
+        }
+
+        public static OrdinalDirectionMask ToMask(this OrdinalDirection direction)
+        {
+            switch (direction)
+            {
+                case OrdinalDirection.North:
+                    return OrdinalDirectionMask.North;
+                case OrdinalDirection.NorthEast:
+                    return OrdinalDirectionMask.NorthEast;
+                case OrdinalDirection.East:
+                    return OrdinalDirectionMask.East;
+                case OrdinalDirection.SouthEast:
+                    return OrdinalDirectionMask.SouthEast;
+                case OrdinalDirection.South:
+                    return OrdinalDirectionMask.South;
+                case OrdinalDirection.SouthWest:
+                    return OrdinalDirectionMask.SouthWest;
+                case OrdinalDirection.West:
+                    return OrdinalDirectionMask.West;
+                case OrdinalDirection.NorthWest:
+                    return OrdinalDirectionMask.NorthWest;
+            }
+
+            throw new Exception($"{nameof(OrdinalDirection)} {direction} is not handled");
+        }
+
+        public static CardinalDirection ToCardinalDirection(this OrdinalDirection direction)
+        {
+            OrdinalDirection newDirection = direction;
+
+            if (direction.IsDiagonal())
+            {
+                newDirection = newDirection.RotateClockwise();
+            }
+
+            switch (newDirection)
+            {
+                case OrdinalDirection.East:
+                    return CardinalDirection.East;
+                case OrdinalDirection.South:
+                    return CardinalDirection.South;
+                case OrdinalDirection.West:
+                    return CardinalDirection.West;
+                default:
+                    return CardinalDirection.North;
+            }
+        }
+
+        public static bool IsHorizontal(this OrdinalDirection direction) =>
+            direction == OrdinalDirection.East || direction == OrdinalDirection.West;
+
+        public static bool IsVertical(this OrdinalDirection direction) =>
+            direction == OrdinalDirection.North || direction == OrdinalDirection.South;
+
+        public static bool IsDiagonal(this OrdinalDirection direction) =>
+            direction == OrdinalDirection.NorthEast || direction == OrdinalDirection.NorthWest
+                                             || direction == OrdinalDirection.SouthWest
+                                             || direction == OrdinalDirection.SouthEast;
+    }
+
+    [Flags]
+    public enum OrdinalDirectionMask
+    {
+        None = 0,
+        North = 1,
+        NorthEast = 2,
+        East = 4,
+        SouthEast = 8,
+        South = 16,
+        SouthWest = 32,
+        West = 64,
+        NorthWest = 128
+    }
+
+    public static class OrdinalDirectionMaskExtensions
+    {
+        public static bool Contains(this OrdinalDirectionMask mask, OrdinalDirection direction) =>
+            (mask & direction.ToMask()) != 0;
+
+        public static IEnumerable<OrdinalDirectionMask> Split(this OrdinalDirectionMask mask)
+        {
+            List<OrdinalDirectionMask> enumerable = new List<OrdinalDirectionMask>(4);
+
+            if ((mask & OrdinalDirectionMask.North) != 0)
+            {
+                enumerable.Add(OrdinalDirectionMask.North);
+            }
+
+            if ((mask & OrdinalDirectionMask.NorthEast) != 0)
+            {
+                enumerable.Add(OrdinalDirectionMask.NorthEast);
+            }
+
+            if ((mask & OrdinalDirectionMask.East) != 0)
+            {
+                enumerable.Add(OrdinalDirectionMask.East);
+            }
+
+            if ((mask & OrdinalDirectionMask.SouthEast) != 0)
+            {
+                enumerable.Add(OrdinalDirectionMask.SouthEast);
+            }
+
+            if ((mask & OrdinalDirectionMask.South) != 0)
+            {
+                enumerable.Add(OrdinalDirectionMask.South);
+            }
+
+            if ((mask & OrdinalDirectionMask.SouthWest) != 0)
+            {
+                enumerable.Add(OrdinalDirectionMask.SouthWest);
+            }
+
+            if ((mask & OrdinalDirectionMask.West) != 0)
+            {
+                enumerable.Add(OrdinalDirectionMask.West);
+            }
+
+            if ((mask & OrdinalDirectionMask.NorthWest) != 0)
+            {
+                enumerable.Add(OrdinalDirectionMask.NorthWest);
+            }
+
+            return enumerable;
+        }
+
+        public static IEnumerable<OrdinalDirection> GetDirections(this OrdinalDirectionMask mask)
+        {
+            List<OrdinalDirection> enumerable = new List<OrdinalDirection>(4);
+
+            if ((mask & OrdinalDirectionMask.North) != 0)
+            {
+                enumerable.Add(OrdinalDirection.North);
+            }
+
+            if ((mask & OrdinalDirectionMask.NorthEast) != 0)
+            {
+                enumerable.Add(OrdinalDirection.NorthEast);
+            }
+
+            if ((mask & OrdinalDirectionMask.East) != 0)
+            {
+                enumerable.Add(OrdinalDirection.East);
+            }
+
+            if ((mask & OrdinalDirectionMask.SouthEast) != 0)
+            {
+                enumerable.Add(OrdinalDirection.SouthEast);
+            }
+
+            if ((mask & OrdinalDirectionMask.South) != 0)
+            {
+                enumerable.Add(OrdinalDirection.South);
+            }
+
+            if ((mask & OrdinalDirectionMask.SouthWest) != 0)
+            {
+                enumerable.Add(OrdinalDirection.SouthWest);
+            }
+
+            if ((mask & OrdinalDirectionMask.West) != 0)
+            {
+                enumerable.Add(OrdinalDirection.West);
+            }
+
+            if ((mask & OrdinalDirectionMask.NorthWest) != 0)
+            {
+                enumerable.Add(OrdinalDirection.NorthWest);
+            }
+
+            return enumerable;
+        }
+
+        public static bool HasVertical(this OrdinalDirectionMask mask) =>
+            (mask & OrdinalDirectionMask.North) != 0 || (mask & OrdinalDirectionMask.South) != 0;
+
+        public static bool HasHorizontal(this OrdinalDirectionMask mask) =>
+            (mask & OrdinalDirectionMask.East) != 0 || (mask & OrdinalDirectionMask.West) != 0;
+
+        public static bool HasDiagonal(this OrdinalDirectionMask mask) =>
+            (mask & OrdinalDirectionMask.NorthEast) != 0 || (mask & OrdinalDirectionMask.NorthWest) != 0
+                                                  || (mask & OrdinalDirectionMask.SouthEast) != 0
+                                                  || (mask & OrdinalDirectionMask.SouthWest) != 0;
+    }
+
+    public static class OrdinalDirectionUtil
+    {
+        public static OrdinalDirection From(Vector2 fromPos, Vector2 toPos)
+        {
+            Vector2 vector = (toPos - fromPos).normalized;
+            float angle = Vector2.SignedAngle(Vector2.up, vector);
+
+            if (angle >= 22.5f && angle < 67.5f)
+            {
+                return OrdinalDirection.NorthWest;
+            }
+
+            if (angle >= 67.5f && angle < 112.5f)
+            {
+                return OrdinalDirection.West;
+            }
+
+            if (angle >= 112.5f && angle < 157.5f)
+            {
+                return OrdinalDirection.SouthWest;
+            }
+
+            if (angle < -22.5f && angle >= -67.5f)
+            {
+                return OrdinalDirection.NorthEast;
+            }
+
+            if (angle < -67.5f && angle >= -112.5f)
+            {
+                return OrdinalDirection.East;
+            }
+
+            if (angle < -112.5f && angle >= -157.5f)
+            {
+                return OrdinalDirection.SouthEast;
+            }
+
+            if (angle >= -22.5f && angle < 22.5f)
+            {
+                return OrdinalDirection.North;
+            }
+
+            return OrdinalDirection.South;
+        }
+
+        public static OrdinalDirectionMask GetMaskFrom(params OrdinalDirection[] directions) => 
+            GetMaskFrom(directions.AsEnumerable());
+
+        public static OrdinalDirectionMask GetMaskFrom(IEnumerable<OrdinalDirection> directions)
+        {
+            OrdinalDirectionMask mask = OrdinalDirectionMask.None;
+
+            foreach (OrdinalDirection direction in directions)
+            {
+                mask |= direction.ToMask();
+            }
+
+            return mask;
+        }
+    }
+}
