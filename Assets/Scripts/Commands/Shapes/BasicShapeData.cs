54d678ee085bdf36558f52b1b1770d32319288bf
A-G-K <agk8097@gmail.com>
2021-05-25 19:33:45 +1000

    BasicShapeData now supports line of sight shapes

--- Assets/Scripts/Commands/Shapes/BasicShapeData.cs
+++ Assets/Scripts/Commands/Shapes/BasicShapeData.cs
@@ -8,8 +8,7 @@ using Utility;
 
 namespace Commands.Shapes
 {
-    [CreateAssetMenu(fileName = "BasicShapeData", menuName = "ScriptableObjects/ShapeData", 
-        order = 0)]
+    [CreateAssetMenu(fileName = "BasicShapeData", menuName = "ScriptableObjects/ShapeData", order = 0)]
     public class BasicShapeData : ScriptableObject, IShape
     {
         [Serializable]
@@ -19,37 +18,37 @@ namespace Commands.Shapes
             public List<Vector2Int> coordinates;
             [Tooltip("This only works for cardinal directions")]
             public bool autoRotateFromNorth;
+            [Tooltip("This will try to raycast on the grid towards the first found unit")]
+            public bool isLineOfSight;
         }
 
         [SerializeField] private List<ShapePart> shapeParts;
+
         [Tooltip("This only works for shapes with no specific direction")]
-        [SerializeField, HideInInspector] private bool shouldFollowMouse;
+        [SerializeField, HideInInspector]
+        private bool shouldFollowMouse;
 
         public bool IsDiagonalShape => shapeParts.Any(p => p.direction.HasDiagonal());
 
-        public bool HasNoDirection =>
-            shapeParts.All(p => p.direction == OrdinalDirectionMask.None);
+        public bool HasNoDirection => shapeParts.All(p => p.direction == OrdinalDirectionMask.None);
 
-        public IEnumerable<Vector2Int> GetHighlightedCells(Vector2 targetVector) =>
-            GetAffectedCoordinates(targetVector);
+        public bool IsLineOfSight => shapeParts.All(p => p.isLineOfSight);
+
+        public IEnumerable<Vector2Int> GetHighlightedCells(Vector2Int originCoordinate, Vector2 targetVector) =>
+            GetAffectedCoordinates(originCoordinate, targetVector);
 
         public IEnumerable<IUnit> GetTargets(Vector2Int originCoordinate, Vector2 targetVector)
         {
             GridManager gridManager = ManagerLocator.Get<GridManager>();
 
-            IEnumerable<Vector2Int> coordinates = GetAffectedCoordinates(targetVector);
-
-            if (HasNoDirection && shouldFollowMouse)
-            {
-                coordinates = coordinates.Select(c => c + originCoordinate);
-            }
+            IEnumerable<Vector2Int> coordinates = GetAffectedCoordinates(originCoordinate, targetVector);
 
             throw new NotImplementedException();
             // TODO how do you get a unit from a coordinate (a.k.a grid position)?
             //return cells.Select(coor => ?????);
         }
 
-        private IEnumerable<Vector2Int> GetAffectedCoordinates(Vector2 targetVector)
+        private IEnumerable<Vector2Int> GetAffectedCoordinates(Vector2Int originCoordinate, Vector2 targetVector)
         {
             bool isDiagonalShape = IsDiagonalShape;
 
@@ -59,12 +58,35 @@ namespace Commands.Shapes
                 ? OrdinalDirectionUtility.From(Vector2.zero, targetVector)
                 : CardinalDirectionUtility.From(Vector2.zero, targetVector).ToOrdinalDirection();
 
-            return shapeParts.Where(p => p.direction.Contains(direction)).SelectMany(p =>
-                p.autoRotateFromNorth && isDiagonalShape
-                    ? p.coordinates.Select(c =>
-                        CardinalDirectionUtility.RotateVector2Int(c, CardinalDirection.North,
-                            direction.ToCardinalDirection()))
-                    : p.coordinates);
+            IEnumerable<Vector2Int> affectedCoordinates = shapeParts.
+                Where(p => p.direction.Contains(direction)).SelectMany(p =>
+                {
+                    IEnumerable<Vector2Int> coordinates = p.coordinates;
+
+                    if (p.autoRotateFromNorth && isDiagonalShape)
+                    {
+                        coordinates = coordinates.Select(c =>
+                            CardinalDirectionUtility.RotateVector2Int(c, CardinalDirection.North,
+                                direction.ToCardinalDirection()));
+                    }
+
+                    if (p.isLineOfSight)
+                    {
+                        // TODO somehow do linecasting or raycasting from somewhere relative to origin
+                        Vector2Int hitVector = Vector2Int.zero;
+
+                        coordinates = coordinates.Select(c => c + hitVector);
+                    }
+
+                    return coordinates;
+                });
+
+            if (HasNoDirection && shouldFollowMouse)
+            {
+                affectedCoordinates = affectedCoordinates.Select(c => c + originCoordinate);
+            }
+
+            return affectedCoordinates;
         }
     }
 }
--- Assets/Scripts/Commands/Shapes/IShape.cs
+++ Assets/Scripts/Commands/Shapes/IShape.cs
@@ -11,7 +11,7 @@ namespace Commands.Shapes
         /// Get all the cells at which this shape will hit. If this is not an AoE shape, it may just
         /// be empty.
         /// </summary>
-        public IEnumerable<Vector2Int> GetHighlightedCells(Vector2 targetVector);
+        public IEnumerable<Vector2Int> GetHighlightedCells(Vector2Int originCoordinate, Vector2 targetVector);
 
         public IEnumerable<IUnit> GetTargets(Vector2Int originCoordinate, Vector2 targetVector);
     }
