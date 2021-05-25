67bbfda2837709960915b13efcce99037b2b9f55
A-G-K <agk8097@gmail.com>
2021-05-26 07:07:25 +1000

    Simplify UnitSelectedCommand, removing delegates
    We're removing delegates here because there should already be some way to listen to the command anyways. We also don't need to care about the current unit in the current turn.

--- Assets/Scripts/Commands/UnitSelectedCommand.cs
+++ Assets/Scripts/Commands/UnitSelectedCommand.cs
@@ -5,26 +5,16 @@ namespace Commands
 {
     public class UnitSelectedCommand : Command
     {
-        //Currently unused idk if it should be delegate or command.
-        public IUnit currentUnit;
-        
-        public static IUnit CurrentUnit => ManagerLocator.Get<TurnManager>().CurrentUnit;
-        
-        public delegate void StartTurnUpdate(IUnit unit);
-        
-        public static StartTurnUpdate UnitSelect;
-        
+        public IUnit CurrentUnit { get; }
+
         public UnitSelectedCommand(IUnit unit) : base(unit) 
         {
-            currentUnit = unit;
+            CurrentUnit = unit;
         }
 
         public override void Queue() {}
 
-        public override void Execute() 
-        {
-            UnitSelect(currentUnit);
-        }
+        public override void Execute() {}
 
         public override void Undo() {}
     }
