b8b61b690a4f7797290b3493b4e692d8968cf3bf
A-G-K <agk8097@gmail.com>
2021-06-10 14:24:00 +1000

    Remove Command queueing

--- Assets/Scripts/Managers/CommandManager.cs
+++ Assets/Scripts/Managers/CommandManager.cs
@@ -15,36 +15,9 @@ namespace Managers
         private readonly Dictionary<Type, LinkedList<CommandAction>> executeListeners =
             new Dictionary<Type, LinkedList<CommandAction>>();
 
-        private readonly Dictionary<Type, LinkedList<CommandAction>> queueListeners =
-            new Dictionary<Type, LinkedList<CommandAction>>();
-
         // private List<Command> turnOrder = new List<Command>();
 
         private int currentCommandHistoryIndex;
-
-        public void QueueCommand(Command command)
-        {
-            if (command.Unit is EnemyUnit unit)
-            {
-                Type commandType = command.GetType();
-
-                unit.QueueCommand(command);
-                
-                command.Queue();
-
-                if (queueListeners.ContainsKey(commandType))
-                {
-                    foreach (var action in queueListeners[commandType])
-                    {
-                        action.Invoke(command);
-                    }
-                }
-            }
-            else
-            {
-                throw new Exception($"Unit is not an {nameof(EnemyUnit)}. Cannot queue a command.");
-            }
-        }
 
         public void ExecuteCommand(Command command)
         {
@@ -61,28 +34,6 @@ namespace Managers
             }
             
             currentCommandHistoryIndex = commandHistory.Count - 1;
-        }
-        
-        /// <summary>
-        /// Listen to when the command is queued
-        ///
-        /// <example>
-        /// How to use.
-        /// <code>
-        /// ListenQueueCommand&lt;EndTurnCommand&gt;((cmd) => Debug.Log("Unit turn has ended!"));
-        /// </code>
-        /// </example>
-        /// </summary>
-        public void ListenQueueCommand<T>(CommandAction action) where T : Command
-        {
-            Type commandType = typeof(T);
-            
-            if (!queueListeners.ContainsKey(commandType))
-            {
-                queueListeners[commandType] = new LinkedList<CommandAction>();
-            }
-
-            queueListeners[commandType].AddLast(action);
         }
 
         /// <summary>
@@ -105,37 +56,6 @@ namespace Managers
             }
 
             executeListeners[commandType].AddLast(action);
-        }
-        
-        /// <summary>
-        /// Listen to when the command is queued. Remember to remove the CommandAction after object
-        /// is destroyed to prevent errors and memory leaks.
-        ///
-        /// <example>
-        /// Keep the action stored as a variable somewhere, then remove it when done with it.
-        /// <code>
-        ///     CommandAction action = (cmd) =>
-        ///     {
-        ///         Debug.Log("unit has ended their turn!");
-        ///     }
-        ///     ListenQueueCommand&lt;EndTurnCommand&gt;(action);
-        ///     ...
-        ///     RemoveQueueListener&lt;EndTurnCommand&gt;(action);
-        /// </code>
-        /// </example>
-        /// </summary>
-        public void RemoveQueueListener<T>(CommandAction action) where T : Command
-        {
-            Type commandType = typeof(T);
-            var foundContext = queueListeners[commandType]
-                .FirstOrDefault(a => a == action);
-
-            queueListeners[commandType].Remove(foundContext);
-
-            if (queueListeners[commandType].Count == 0)
-            {
-                queueListeners.Remove(commandType);
-            }
         }
 
         /// <summary>
