b3ef7ca669a281b02f120700611c13daec209b29
A-G-K <agk8097@gmail.com>
2021-06-11 22:30:25 +1000

    Implement catch listeners for ready commands

--- Assets/Scripts/Managers/CommandManager.cs
+++ Assets/Scripts/Managers/CommandManager.cs
@@ -6,8 +6,6 @@ using Units;
 
 namespace Managers
 {
-    public delegate void CommandAction(Command cmd);
-    
     public class CommandManager : Manager
     { 
         /// <summary>
@@ -19,8 +17,15 @@ namespace Managers
         /// Basically keeps tracks of what listeners need to be called when some command is executed.
         /// Stores all listeners in a graph data structure for efficiency.
         /// </summary>
-        private readonly Dictionary<Type, LinkedList<CommandAction>> executeListeners =
-            new Dictionary<Type, LinkedList<CommandAction>>();
+        private readonly Dictionary<Type, LinkedList<Delegate>> listeners =
+            new Dictionary<Type, LinkedList<Delegate>>();
+
+        /// <summary>
+        /// Used for catch listeners, keeps track of all commands that are executed after a given
+        /// catch listener is registered.
+        /// </summary>
+        private readonly Dictionary<Delegate, List<Command>> caughtCommands =
+            new Dictionary<Delegate, List<Command>>();
 
         /// <summary>
         /// Execute the given command. Anything listening to this type of command would be notified.
@@ -39,66 +44,216 @@ namespace Managers
             
             command.Execute();
             
-            if (executeListeners.ContainsKey(commandType))
+            if (listeners.ContainsKey(commandType))
             {
-                foreach (var action in executeListeners[commandType])
+                // We to remove listeners as our final step to prevent array modification exceptions
+                var catchingListenersToRemove = new List<Delegate>();
+                
+                foreach (var action in listeners[commandType])
                 {
-                    action.Invoke(command);
+                    // Check if the action is a catch listener
+                    if (caughtCommands.ContainsKey(action))
+                    {
+                        // If there is already a command with the current command type, remove it
+                        caughtCommands[action].RemoveAll(c => c.GetType() == commandType);
+                        
+                        // The current command type is now fulfilled for that catch listener
+                        caughtCommands[action].Add(command);
+                        
+                        var requiredCommandTypes = action.GetType().GetGenericArguments();
+                        
+                        // Check if all the command requirements has been met
+                        // Basically compare if 2 different lists are the same
+                        bool isNotReady = caughtCommands[action]
+                            .Select(cmd => cmd.GetType())
+                            .Except(requiredCommandTypes)
+                            .Any();
+
+                        if (!isNotReady)
+                        {
+                            catchingListenersToRemove.Add(action);
+                            // Need to cast to Array<object> for it to pass in parameters properly
+                            action.DynamicInvoke(caughtCommands[action].ToArray<object>());
+                        }
+                    }
+                    // Otherwise just invoke it normally
+                    else
+                    { 
+                        action.DynamicInvoke(command);
+                    }
                 }
+                
+                catchingListenersToRemove.ForEach(RemoveCatchListener);
             }
         }
 
         /// <summary>
-        /// Listen to when the command is executed
+        /// Listen to when the command is executed. Do something every time the command is executed.
         ///
         /// <example>
         /// How to use.
         /// <code>
-        ///     ListenExecuteCommand&lt;EndTurnCommand&gt;((cmd) => Debug.Log("Unit turn has ended!"));
+        ///     ListenCommand&lt;EndTurnCommand&gt;((cmd) => Debug.Log("Unit turn has ended!"));
         /// </code>
         /// </example>
         /// </summary>
-        public void ListenCommand<T>(CommandAction action) where T : Command
+        public void ListenCommand<T>(Action<T> action) where T : Command
         {
-            Type commandType = typeof(T);
-            
-            if (!executeListeners.ContainsKey(commandType))
-            {
-                executeListeners[commandType] = new LinkedList<CommandAction>();
-            }
-
-            executeListeners[commandType].AddLast(action);
+            RegisterListener(typeof(T), action);
         }
 
         /// <summary>
-        /// Listen to when the command is executed. We need to remove the CommandAction after object
-        /// is destroyed to prevent errors and memory leaks. E.g for not having the action be called
-        /// after some GameObject is no longer in use.
+        /// Stop listening to a command execution. We need to remove the Action object when we 
+        /// don't need it to respond to command execution anymore. E.g We need to remove the
+        /// Action object after some GameObject is no longer in use. Also to prevent errors and memory leaks
         ///
         /// <example>
         /// Keep the action stored as a variable somewhere, then remove it when done with it.
         /// <code>
-        ///     CommandAction action = (cmd) =>
+        ///     Action&lt;EndTurnCommand&gt; action = (cmd) =>
         ///     {
         ///         Debug.Log("unit has ended their turn!");
         ///     }
-        ///     ListenExecuteCommand&lt;EndTurnCommand&gt;(action);
+        ///     ListenCommand&lt;EndTurnCommand&gt;(action);
         ///     ...
-        ///     RemoveExecuteListener&lt;EndTurnCommand&gt;(action);
+        ///     UnlistenCommand&lt;EndTurnCommand&gt;(action);
         /// </code>
         /// </example>
         /// </summary>
-        public void UnlistenCommand<T>(CommandAction action) where T : Command
+        public void UnlistenCommand<T>(Action<T> action) where T : Command
         {
             Type commandType = typeof(T);
-            var foundContext = executeListeners[commandType]
-                .FirstOrDefault(a => a == action);
+            var foundListener = listeners[commandType]
+                .FirstOrDefault(a => a == (Delegate) action);
 
-            executeListeners[commandType].Remove(foundContext);
+            // Check if the delegate was actually added in the list in the first place
+            if (foundListener != null)
+                RemoveListener(commandType, foundListener);
+        }
 
-            if (executeListeners[commandType].Count == 0)
+        /// <summary>
+        /// Do something <b>only once</b>, when a command is executed.
+        ///
+        /// <example>
+        /// How to use.
+        /// <code>
+        ///     CatchCommand&lt;PlayerUnitsReadyCommand&gt;((cmd) => Debug.Log("Player units are ready!"));
+        /// </code>
+        /// </example>
+        /// </summary>
+        public void CatchCommand<T>(Action<T> action) where T : Command => RegisterCatchListener(action);
+
+        /// <summary>
+        /// Do something <b>only once</b>, when a command is executed.
+        ///
+        /// <example>
+        /// How to use.
+        /// <code>
+        ///     CatchCommand&lt;PlayerUnitsReadyCommand&gt;((cmd) => Debug.Log("Player units are ready!"));
+        /// </code>
+        /// </example>
+        /// </summary>
+        public void CatchCommand<T1, T2>(Action<T1, T2> action)
+            where T1 : Command 
+            where T2 : Command =>
+            RegisterCatchListener(action);
+
+        /// <summary>
+        /// Do something <b>only once</b>, when a command is executed.
+        ///
+        /// <example>
+        /// How to use.
+        /// <code>
+        ///     CatchCommand&lt;PlayerUnitsReadyCommand&gt;((cmd) => Debug.Log("Player units are ready!"));
+        /// </code>
+        /// </example>
+        /// </summary>
+        public void CatchCommand<T1, T2, T3>(Action<T1, T2, T3> action)
+            where T1 : Command 
+            where T2 : Command 
+            where T3 : Command =>
+            RegisterCatchListener(action);
+
+        /// <summary>
+        /// Do something <b>only once</b>, when a command is executed.
+        ///
+        /// <example>
+        /// How to use.
+        /// <code>
+        ///     CatchCommand&lt;PlayerUnitsReadyCommand&gt;((cmd) => Debug.Log("Player units are ready!"));
+        /// </code>
+        /// </example>
+        /// </summary>
+        public void CatchCommand<T1, T2, T3, T4>(Action<T1, T2, T3, T4> action)
+            where T1 : Command 
+            where T2 : Command 
+            where T3 : Command 
+            where T4 : Command =>
+            RegisterCatchListener(action);
+
+        /// <summary>
+        /// Do something <b>only once</b>, when a command is executed.
+        ///
+        /// <example>
+        /// How to use.
+        /// <code>
+        ///     CatchCommand&lt;PlayerUnitsReadyCommand&gt;((cmd) => Debug.Log("Player units are ready!"));
+        /// </code>
+        /// </example>
+        /// </summary>
+        public void CatchCommand<T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5> action)
+            where T1 : Command
+            where T2 : Command
+            where T3 : Command
+            where T4 : Command
+            where T5 : Command =>
+            RegisterCatchListener(action);
+
+        private void RegisterCatchListener(Delegate action)
+        {
+            if (caughtCommands.ContainsKey(action))
+                return;
+            
+            Type[] commandTypes = action.GetType().GetGenericArguments();
+
+            foreach (Type commandType in commandTypes)
             {
-                executeListeners.Remove(commandType);
+                RegisterListener(commandType, action);
+            }
+            
+            caughtCommands.Add(action, new List<Command>());
+        }
+
+        private void RemoveCatchListener(Delegate action)
+        {
+            Type[] commandTypes = action.GetType().GetGenericArguments();
+
+            foreach (Type commandType in commandTypes)
+            {
+                RemoveListener(commandType, action);
+            }
+            
+            caughtCommands.Remove(action);
+        }
+
+        private void RegisterListener(Type commandType, Delegate action)
+        {
+            if (listeners.ContainsKey(commandType) && listeners[commandType].Contains(action))
+                return;
+            
+            if (!listeners.ContainsKey(commandType))
+                listeners[commandType] = new LinkedList<Delegate>();
+            
+            listeners[commandType].AddLast(action);
+        }
+
+        private void RemoveListener(Type commandType, Delegate action)
+        {
+            listeners[commandType].Remove(action);
+
+            if (listeners[commandType].Count == 0)
+            {
+                listeners.Remove(commandType);
             }
         }
 
