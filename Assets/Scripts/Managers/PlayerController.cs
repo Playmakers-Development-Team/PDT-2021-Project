de47c5989886a1937cc1acab44c9f6654623be1a
H A <hugoa3520@gmail.com>
2021-06-16 22:58:16 +1000

    minor change

--- Assets/Scripts/Managers/PlayerController.cs
+++ Assets/Scripts/Managers/PlayerController.cs
@@ -15,7 +15,7 @@ namespace Managers
             GameObject playerPrefab = (GameObject)Resources.Load("Prefabs/GridObjects/PlayerPlaceholder", typeof(GameObject));
             
             PlayerManager playerManager = ManagerLocator.Get<PlayerManager>();
-            //playerManager.Spawn(playerPrefab, Vector2Int.zero);
+            //playerManager.Spawn(playerPrefab, Vector2Int.zero); //TEST THIS
             //playerManager.Spawn(playerPrefab, Vector2Int.up);
             ManagerLocator.Get<CommandManager>().ExecuteCommand(new PlayerUnitsReadyCommand());
         }
