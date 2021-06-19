4721b9d7963719370e0f7e1838d20619c3b731f1
Sam <sam.huang852@gmail.com>
2021-06-19 15:27:06 +1000

    Q/A Review

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -15,7 +15,7 @@ namespace Units
     {
         [SerializeField] protected T data;
         
-        public TenetType tenet => data.tenet;
+        public TenetType Tenet => data.tenet;
         public ValueStat MovementActionPoints => data.movementActionPoints;
         public ValueStat Speed => data.speed;
         public ModifierStat DealDamageModifier => data.dealDamageModifier;
