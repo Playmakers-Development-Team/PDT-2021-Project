12b4dda13c59226d96ee8325361e96e40a5ad2c7
Jordan Tama <Jordan.Hamlin6@gmail.com>
2021-07-10 19:39:56 +1000

    Added damage text to unit UI.

--- Assets/Scripts/UI/UnitUI.cs
+++ Assets/Scripts/UI/UnitUI.cs
@@ -1,4 +1,7 @@
-﻿using GridObjects;
+﻿using System;
+using Cysharp.Threading.Tasks;
+using GridObjects;
+using TMPro;
 using Units;
 using UnityEngine;
 
@@ -7,19 +10,51 @@ namespace UI
     public class UnitUI : Element
     {
         [SerializeField] private GridObject unitGridObject;
+        [SerializeField] private TextMeshProUGUI damageText;
+        [SerializeField] private float damageTextDuration;
 
+        private float lastHealth;
         private IUnit unit;
 
 
         protected override void OnAwake()
         {
+            
             unit = unitGridObject as IUnit;
             
             if (unit == null)
                 DestroyImmediate(gameObject);
+            
+            // TODO: Listen to unit.onTakeDamage!
         }
 
-        
+        private void LateUpdate()
+        {
+            float health = unit.Health.HealthPoints.Value;
+            if (health < lastHealth)
+                OnTakeDamage((int) (lastHealth - health));
+            lastHealth = health;
+        }
+
+
         public void OnClick() => manager.unitSelected.Invoke(unit);
+
+        private async void OnTakeDamage(int damage)
+        {
+            await DamageTextDisplay(damage);
+            DamageTextHide();
+        }
+
+        private UniTask DamageTextDisplay(int damage)
+        {
+            damageText.text = damage.ToString();
+            damageText.enabled = true;
+            return UniTask.Delay((int) damageTextDuration * 1000);
+        }
+
+        private void DamageTextHide()
+        {
+            damageText.enabled = false;
+        }
     }
 }
