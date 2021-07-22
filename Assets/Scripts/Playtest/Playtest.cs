71e38f7e96cb272409b402cb86fd721db213911f
Francisco <francisco.castro3030@gmail.com>
2021-07-22 20:39:38 +1000

    formatted google forms sheet

--- Assets/Scripts/Playtest/Playtest.cs
+++ Assets/Scripts/Playtest/Playtest.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Commands;
 using Cysharp.Threading.Tasks;
 using Managers;
+using Turn;
+using Turn.Commands;
+using Units;
 using Units.Commands;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -15,32 +20,77 @@ namespace Playtest
         [SerializeField] private PlaytestData data;
         
         private CommandManager commandManager;
+        private TurnManager turnManager;
+        private UnitManager unitManager;
 
+        #region EntryFields
+
+        private const string levelPlayedField = "entry.295363220";
+        private const string initialUnitStatField = "entry.512851580";
+        private const string initialTimelineField = "entry.887732368";
+
+        #endregion
+        
+        
         private const string url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdnv_MhoRAG5l7yFEVhFOvBLpIgKGynzoiHUhjP7f19L-99Fw/formResponse";
-
+        
         private void InitialiseStats()
         {
+            #region DataInitialise
+            
+            data.InitialUnits = "";
             data.TimesMoved = 0;
-            data.activeScene = SceneManager.GetActiveScene();
+            data.activeScene = SceneManager.GetActiveScene().name;
+        
+            foreach (var unit in unitManager.AllUnits)
+            {
+                data.InitialUnits = data.InitialUnits +  unit.Name + " HP: " +unit.HealthStat
+                .Value + " ATK: " + unit.AttackStat.Value + " DEF: " + unit.DefenceStat.Value + " MP: " +
+                                    unit.MovementPoints.Value + " SPD: " + unit.SpeedStat.Value +
+                                    Environment.NewLine;
+            }
+            
+            foreach (var unit in turnManager.CurrentTurnQueue)
+                data.InitialUnitOrder = data.InitialUnitOrder +  unit.Name + Environment.NewLine;
+            
+            #endregion
+
+            #region EntriesInitialisation
+            
+            if(data.Entries.Count != 0)
+                 data.Entries.Clear();
+            
+            data.Entries.Add(new Tuple<string,string>(data.activeScene,levelPlayedField));
+            data.Entries.Add(new Tuple<string,string>(data.InitialUnits,initialUnitStatField));
+            data.Entries.Add(new Tuple<string,string>(data.InitialUnitOrder,initialTimelineField));
+
+            #endregion
+            
         }
     
         private void Awake()
         {
-            //InitialiseStats();
             commandManager = ManagerLocator.Get<CommandManager>();
+            turnManager = ManagerLocator.Get<TurnManager>();
+            unitManager = ManagerLocator.Get<UnitManager>();
         }
 
         private void Start()
         {
+            commandManager.ListenCommand<TurnQueueCreatedCommand>(cmd => InitialiseStats());
+
             commandManager.ListenCommand<StartMoveCommand>(cmd =>
             {
                 data.TimesMoved++;
             });
 
-            if (Application.isPlaying)
-                InitialiseStats();
+            if (!Application.isPlaying)
+                PostAll(data.Entries);
             else
-                PostAll();
+                InitialiseStats();
+            
+            
+              
         }
 
         
@@ -55,16 +105,17 @@ namespace Playtest
 
         #region PostData
 
-        private void PostAll()
+        private async void PostAll(List<Tuple<string,string>> entries)
         {
-            Post("entry.290157031","Level Playing: " + data.activeScene);
-            Post("entry.290157031","Level Playing: " + data.activeScene);
-            Post("entry.290157031","Level Playing: " + data.activeScene);
-            Post("entry.290157031","Level Playing: " + data.activeScene);
-            Post("entry.290157031","Level Playing: " + data.activeScene);
-
+            WWWForm form = new WWWForm();
             
+            foreach (Tuple<string, string> entry in entries)
+                form.AddField(entry.Item2, entry.Item1);
+            
+            UnityWebRequest www = UnityWebRequest.Post(url, form);
+            await www.SendWebRequest();
         }
+        
         #endregion
     }
 }
--- Assets/Scripts/Playtest/PlaytestData.cs
+++ Assets/Scripts/Playtest/PlaytestData.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Units;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,10 +12,14 @@ namespace Playtest
     public class PlaytestData : ScriptableObject
     {
         
-        public Scene activeScene;
+        public string activeScene;
         [field : SerializeField]public int TimesMoved { get; set; }
-
         
+        public string InitialUnits { get; set; }
+
+        public List<Tuple<string, string>> Entries = new List<Tuple<string, string>>();
+
+        public string InitialUnitOrder { get; set; }
 
 
     }
