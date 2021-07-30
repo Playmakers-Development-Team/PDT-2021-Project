b6aae4b2cb91543f9d6168e25f41b17c442f2ff7
Jordan Tama <Jordan.Hamlin6@gmail.com>
2021-07-30 18:42:49 +1000

    End turn button now functional.

--- Assets/Scripts/UI/Game/UnitPanels/EndTurnButton.cs
+++ Assets/Scripts/UI/Game/UnitPanels/EndTurnButton.cs
@@ -1,4 +1,8 @@
-﻿using UI.Core;
+﻿using Commands;
+using Managers;
+using Turn;
+using Turn.Commands;
+using UI.Core;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,12 +13,18 @@ namespace UI.Game.UnitPanels
         [SerializeField] private Animator animator;
         [SerializeField] private EventTrigger eventTrigger;
         
+        private CommandManager commandManager;
+        private TurnManager turnManager;
+        
         private static readonly int normalId = Animator.StringToHash("Normal");
         private static readonly int highlightedId = Animator.StringToHash("Highlighted");
 
 
         protected override void OnComponentAwake()
         {
+            commandManager = ManagerLocator.Get<CommandManager>();
+            turnManager = ManagerLocator.Get<TurnManager>();
+            
             EventTrigger.Entry enter = new EventTrigger.Entry {eventID = EventTriggerType.PointerEnter};
             enter.callback.AddListener(data => animator.SetTrigger(highlightedId));
             eventTrigger.triggers.Add(enter);
@@ -22,6 +32,16 @@ namespace UI.Game.UnitPanels
             EventTrigger.Entry exit = new EventTrigger.Entry {eventID = EventTriggerType.PointerExit};
             exit.callback.AddListener(data => animator.SetTrigger(normalId));
             eventTrigger.triggers.Add(exit);
+
+            EventTrigger.Entry pointerClick = new EventTrigger.Entry {eventID = EventTriggerType.PointerClick};
+            pointerClick.callback.AddListener(data =>
+            {
+                if (turnManager.ActingPlayerUnit == null)
+                    return;
+                
+                commandManager.ExecuteCommand(new EndTurnCommand(turnManager.ActingPlayerUnit));
+            });
+            eventTrigger.triggers.Add(pointerClick);
         }
 
         protected override void Subscribe() {}
