d64618d5d1a07eb978e5a97def8dee9bc3eb918c
Lachlan Garrity <lachlangarrity@gmail.com>
2021-08-01 18:26:14 +1000

    EnemyDataDrawer Added

---
+++ Assets/Editor/EnemyUnitDataDrawer.cs
@@ -0,0 +1,48 @@
+using Commands;
+using Managers;
+using Units;
+using Units.Commands;
+using Units.Enemies;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomPropertyDrawer(typeof(EnemyUnitData))]
+    public class EnemyUnitDataDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var btnRect = new Rect(position.width / 4, position.y + 5, position.width / 2, 40);
+            var defaultRect = new Rect(position.x + 5, position.y + 40, position.width,
+                position.height);
+
+            EditorGUI.PropertyField(defaultRect, property, label, true);
+
+            if (GUI.Button(btnRect, "UPDATE UNIT STAT UI"))
+            {
+                if (Application.isPlaying)
+                {
+                    IUnit iunit = (IUnit) property.serializedObject.targetObject;
+                    CommandManager commandManager = ManagerLocator.Get<CommandManager>();
+                    commandManager.ExecuteCommand(
+                        new AttackChangeCommand(iunit, (int) iunit.Attack.BaseAdder));
+                    commandManager.ExecuteCommand(
+                        new MovementActionPointChangedCommand(iunit,
+                            iunit.MovementActionPoints.BaseValue));
+                    commandManager.ExecuteCommand(
+                        new SpeedChangedCommand(iunit, iunit.Speed.BaseValue));
+                }
+                else
+                {
+                    Debug.Log("Cannot update unless the application is running");
+                }
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return base.GetPropertyHeight(property, label) + 540;
+        }
+    }
+}
--- Assets/Editor/PlayerUnitDataDrawer.cs
+++ Assets/Editor/PlayerUnitDataDrawer.cs
@@ -3,42 +3,41 @@ using Managers;
 using Units;
 using Units.Commands;
 using Units.Players;
-using Units.Stats;
 using UnityEditor;
 using UnityEngine;
 
 namespace Editor
 {
     [CustomPropertyDrawer(typeof(PlayerUnitData))]
-    public class UnitDataDrawer: PropertyDrawer
+    public class PlayerUnitDataDrawer : PropertyDrawer
     {
-        private SerializedProperty health, movement, speed;
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            
             var btnRect = new Rect(position.width / 4, position.y + 5, position.width / 2, 40);
-            var defaultRect = new Rect(position.x + 5, position.y + 40, position.width, position.height);
-            
+            var defaultRect = new Rect(position.x + 5, position.y + 40, position.width,
+                position.height);
+
             EditorGUI.PropertyField(defaultRect, property, label, true);
-            speed = property.FindPropertyRelative("speed");
 
             if (GUI.Button(btnRect, "UPDATE UNIT STAT UI"))
             {
                 if (Application.isPlaying)
                 {
-                    
-                    IUnit iunit = (IUnit)property.serializedObject.targetObject;
+                    IUnit iunit = (IUnit) property.serializedObject.targetObject;
                     CommandManager commandManager = ManagerLocator.Get<CommandManager>();
-                    commandManager.ExecuteCommand(new AttackChangeCommand(iunit, (int)iunit.Attack.BaseAdder));
-                    commandManager.ExecuteCommand(new MovementActionPointChangedCommand(iunit, iunit.MovementActionPoints.BaseValue));
-                    commandManager.ExecuteCommand(new SpeedChangedCommand(iunit, iunit.Speed.BaseValue));
+                    commandManager.ExecuteCommand(
+                        new AttackChangeCommand(iunit, (int) iunit.Attack.BaseAdder));
+                    commandManager.ExecuteCommand(
+                        new MovementActionPointChangedCommand(iunit,
+                            iunit.MovementActionPoints.BaseValue));
+                    commandManager.ExecuteCommand(
+                        new SpeedChangedCommand(iunit, iunit.Speed.BaseValue));
                 }
                 else
                 {
                     Debug.Log("Cannot update unless the application is running");
                 }
             }
-            
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
