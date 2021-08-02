07dfcb2002c645eab8b8dc572cd931665b75f1e8
Jack Coggins <voidawake90@gmail.com>
2021-08-02 23:44:53 +1000

    Remove obsolete Stat types
    This may have affected some of the UnitData on the unit prefabs. I did my best to make sure values were copied over to the new properties, but I might have made some mistakes.

--- Assets/Scripts/Units/UnitData.cs
+++ Assets/Scripts/Units/UnitData.cs
@@ -3,101 +3,20 @@ using Abilities;
 using Units.Stats;
 using TenetStatuses;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Units
 {
     public abstract class UnitData
     {
-        [SerializeField] private string name;
-        [SerializeField] private TenetType tenet;
-        [SerializeField] private ValueStat healthPoints;
-
-        [FormerlySerializedAs("movementActionPoints")] [SerializeField]
-        private Stat movementPoints;
-
-        [SerializeField] private ValueStat speed;
-
-        [FormerlySerializedAs("dealDamageModifier")] [SerializeField]
-        private ModifierStat attack;
-
-        [FormerlySerializedAs("takeDamageModifier")] [SerializeField]
-        private ModifierStat defence;
-
-        [SerializeField] private ModifierStat takeKnockbackModifier;
-        [SerializeField] private List<Ability> abilities;
-        
+        [field: SerializeField] public string Name { get; set; }
+        [field: SerializeField] public TenetType Tenet { get; set; }
+        [field: SerializeField] public Stat MovementPoints { get; set; }
+        [field: SerializeField] public List<Ability> Abilities { get; set; }
         [field: SerializeField] public HealthStat HealthValue { get; set; }
         [field: SerializeField] public Stat SpeedStat { get; set; }
         [field: SerializeField] public Stat DefenceStat { get; set; }
         [field: SerializeField] public Stat AttackStat { get; set; }
         [field: SerializeField] public Stat KnockbackStat { get; set; }
-        
         [field: SerializeField] public List<TenetStatus> StartingTenets { get; set; }
-        
-        // TODO: Remove all fields of type "ModifierStat" or "ValueStat".
-        public string Name
-        {
-            get => name;
-            set => name = value;
-        }
-
-        public TenetType Tenet
-        {
-            get => tenet;
-            set => tenet = value;
-        }
-
-        public ValueStat HealthPoints
-        {
-            get => healthPoints;
-            set => healthPoints = value;
-        }
-
-        public Stat MovementPoints
-        {
-            get => movementPoints;
-            set => movementPoints = value;
-        }
-
-        public ValueStat Speed
-        {
-            get => speed;
-            set => speed = value;
-        }
-
-        public ModifierStat Attack
-        {
-            get => attack;
-            set => attack = value;
-        }
-
-        public ModifierStat Defence
-        {
-            get => defence;
-            set => defence = value;
-        }
-
-        public ModifierStat TakeKnockbackModifier
-        {
-            get => takeKnockbackModifier;
-            set => takeKnockbackModifier = value;
-        }
-
-        public List<Ability> Abilities
-        {
-            get => abilities;
-            set => abilities = value;
-        }
-
-        //TODO: Remove this function.
-        public void Initialise()
-        {
-            HealthPoints.Reset();
-            Speed.Reset();
-            Attack.Reset();
-            Defence.Reset();
-            TakeKnockbackModifier.Reset();
-        }
     }
 }
