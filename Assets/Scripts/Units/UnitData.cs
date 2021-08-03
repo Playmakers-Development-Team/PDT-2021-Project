5abbf5a483d71cc309df0a7cdae74e0ad9fcf049
Jack Coggins <voidawake90@gmail.com>
2021-08-03 17:13:32 +1000

    Fix abilities on PlayerUnit prefabs
    Reverted changes to UnitData.

--- Assets/Scripts/Units/UnitData.cs
+++ Assets/Scripts/Units/UnitData.cs
@@ -3,20 +3,50 @@ using Abilities;
 using Units.Stats;
 using TenetStatuses;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Units
 {
     public abstract class UnitData
     {
-        [field: SerializeField] public string Name { get; set; }
-        [field: SerializeField] public TenetType Tenet { get; set; }
-        [field: SerializeField] public Stat MovementPoints { get; set; }
-        [field: SerializeField] public List<Ability> Abilities { get; set; }
+        [SerializeField] private string name;
+        [SerializeField] private TenetType tenet;
+
+        [FormerlySerializedAs("movementActionPoints")] [SerializeField]
+        private Stat movementPoints;
+
+        [SerializeField] private List<Ability> abilities;
+        
         [field: SerializeField] public HealthStat HealthValue { get; set; }
         [field: SerializeField] public Stat SpeedStat { get; set; }
         [field: SerializeField] public Stat DefenceStat { get; set; }
         [field: SerializeField] public Stat AttackStat { get; set; }
         [field: SerializeField] public Stat KnockbackStat { get; set; }
+        
         [field: SerializeField] public List<TenetStatus> StartingTenets { get; set; }
+        
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public TenetType Tenet
+        {
+            get => tenet;
+            set => tenet = value;
+        }
+
+        public Stat MovementPoints
+        {
+            get => movementPoints;
+            set => movementPoints = value;
+        }
+
+        public List<Ability> Abilities
+        {
+            get => abilities;
+            set => abilities = value;
+        }
     }
 }
