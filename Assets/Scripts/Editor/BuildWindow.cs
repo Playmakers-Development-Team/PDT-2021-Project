69116fcf8d626c995a577b1d8bdb028d6b9d6dfc
A-G-K <agk8097@gmail.com>
2021-08-27 06:10:10 +1000

    I made a build tool

---
+++ Assets/Scripts/Editor/BuildWindow.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Game.Map;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Editor
+{
+    public class BuildWindow : EditorWindow
+    {
+        [MenuItem("Window/Build Tool")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<BuildWindow>();
+            window.titleContent = new GUIContent("Build Tool");
+            window.Show();
+        }
+
+        private void CreateGUI()
+        {
+            Label titleLabel = new Label("Build Tool")
+            {
+                style =
+                {
+                    fontSize = 24,
+                    unityFontStyleAndWeight = FontStyle.Bold,
+                    paddingBottom = 15,
+                    paddingTop = 10,
+                    paddingLeft = 15,
+                    paddingRight = 15
+                }
+            };
+
+            TextField versionField = new TextField("Version")
+            {
+                value = "0.X.X"
+            };
+            
+            ObjectField objectField = new ObjectField("Map")
+            {
+                objectType = typeof(MapData),
+                allowSceneObjects = false
+            };
+
+            Toggle devToggle = new Toggle("Development Build");
+
+            var buildButtons = new VisualElement()
+            {
+                style =
+                {
+                    marginTop = 20,
+                    marginBottom = 10,
+                    marginLeft = 5,
+                    marginRight = 5,
+                    backgroundColor = new Color(0.26f, 0.26f, 0.26f),
+                    alignItems = Align.Center,
+                    paddingTop = 10,
+                    paddingBottom = 10,
+                    borderBottomLeftRadius = 10,
+                    borderBottomRightRadius = 10,
+                    borderTopLeftRadius = 10,
+                    borderTopRightRadius = 10
+                }
+            };
+
+            Toggle buildWindows = new Toggle("Build Windows");
+            Toggle buildMac = new Toggle("Build Mac");
+            Toggle buildLinux = new Toggle("Build Linux");
+
+            buildButtons.Add(buildWindows);
+            buildButtons.Add(buildMac);
+            buildButtons.Add(buildLinux);
+
+
+            buildButtons.Add(new Button(() =>
+            {
+                if (buildWindows.value)
+                    BuildPlatform(versionField.value, BuildTarget.StandaloneWindows64, objectField.value as MapData, devToggle.value);
+                
+                if (buildMac.value)
+                    BuildPlatform(versionField.value, BuildTarget.StandaloneOSX, objectField.value as MapData, devToggle.value);
+                
+                if (buildLinux.value)
+                    BuildPlatform(versionField.value, BuildTarget.StandaloneLinux64, objectField.value as MapData, devToggle.value);
+            })
+            {
+                text = "Build",
+                style =
+                {
+                    marginTop = 8,
+                    marginBottom = 8,
+                    width = Length.Percent(50)
+                }
+            });
+
+            Button openFolderButton = new Button(() => 
+                    Application.OpenURL("file://" + Application.dataPath.Replace("Assets", "Builds")))
+            {
+                text = "Open Build Folder",
+                style =
+                {
+                    width = Length.Percent(80),
+                    marginLeft = StyleKeyword.Auto,
+                    marginRight = StyleKeyword.Auto
+                }
+            };
+            
+            rootVisualElement.Add(titleLabel);
+            rootVisualElement.Add(versionField);
+            rootVisualElement.Add(objectField);
+            rootVisualElement.Add(devToggle);
+            rootVisualElement.Add(buildButtons);
+            rootVisualElement.Add(openFolderButton);
+        }
+
+        /// <summary>
+        /// Create a build.
+        /// Kept as a static function so that we can make automated builds.
+        /// </summary>
+        public static void BuildPlatform(string version, BuildTarget buildTarget, MapData mapData, bool developmentBuild = false)
+        {
+            if (mapData == null)
+            {
+                Debug.LogError("Trying to build, but missing a map! Please specify a map!");
+                return;
+            }
+
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = GetRequiredScenes(mapData).ToArray();
+            buildPlayerOptions.locationPathName = $"{GetBuildPath(buildTarget)}-v{version}/Soul Searcher-v{version}";
+            buildPlayerOptions.target = buildTarget;
+            buildPlayerOptions.options = developmentBuild 
+                ? BuildOptions.Development | BuildOptions.AllowDebugging 
+                : BuildOptions.None;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+                Debug.Log("Build Succeeded: " + summary.totalSize / 1000000 + " megabytes");
+
+            if (summary.result == BuildResult.Failed)
+                Debug.LogError("Build Failed!");
+        }
+
+        private static IEnumerable<string> GetRequiredScenes(MapData mapData) => 
+            Enumerable.Empty<string>()
+                // Make sure this is first, so it loads in first
+                .Append(mapData.mapScene.ScenePath)
+                // We need this for some reason, otherwise exiting an encounter won't work
+                .Append("Assets/Scenes/Developer/Map Test.unity") 
+                .Concat(mapData.encounterNodes
+                    .Select(n => n.EncounterData.encounterScene.ScenePath));
+
+        /// <summary>
+        /// Get an appropriate build location
+        /// </summary>
+        private static string GetBuildPath(BuildTarget buildTarget) => buildTarget switch
+        {
+            BuildTarget.StandaloneWindows64 => "Builds/SoulSearcher-WINDOWS",
+            BuildTarget.StandaloneOSX => "Builds/SoulSearcher-MAC",
+            BuildTarget.StandaloneLinux64 => "Builds/SoulSearcher-LINUX",
+             _ => throw new ArgumentException($"Unsupported build target {buildTarget}")
+        };
+    }
+}
