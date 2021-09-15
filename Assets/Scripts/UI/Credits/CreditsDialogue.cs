dd0d476fd5baaea93e250e6f1f42906d91b29062
Jack Coggins <voidawake90@gmail.com>
2021-09-15 17:24:46 +1000

    Credits Dialogue

---
+++ Assets/Scripts/UI/Credits/CreditsDialogue.cs
@@ -0,0 +1,21 @@
+using UI.Core;
+using UnityEngine;
+
+namespace UI.Credits
+{
+    public class CreditsDialogue : Dialogue
+    {
+        protected override void OnClose() {}
+
+        protected override void OnPromote() {}
+
+        protected override void OnDemote() {}
+
+        // TODO: Not sure if this is the standard for getting input.
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                manager.Pop();
+        }
+    }
+}
