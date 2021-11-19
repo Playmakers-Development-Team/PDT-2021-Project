2507c91e3c2544dbe3db2076e53f8edb7b72919a
Jack Coggins <voidawake90@gmail.com>
2021-11-19 23:23:05 +1100

    Add GetVolume function to update volume sliders

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -15,14 +15,22 @@ namespace Audio
         }
 
         public void ChangeVolume(string volume, float value) => AkSoundEngine.SetRTPCValue(volume, value);
-        
+
+        public float GetVolume(string volume)
+        {
+            int type = 1;
+
+            AkSoundEngine.GetRTPCValue(volume, null, 0, out var value, ref type);
+
+            return value;
+        }
+
         /// <summary>
         /// Update the current music of the game
         /// </summary>
-        /// <param name="StateGroup"></param>
+        /// <param name="stateGroup"></param>
         /// <param name="stateName"></param>
-        public void ChangeMusicState(string stateGroup, string stateName) => commandManager.ExecuteCommand(new ChangeMusicStateCommand(stateGroup,stateName));
-        
-        
+        public void ChangeMusicState(string stateGroup, string stateName) =>
+            commandManager.ExecuteCommand(new ChangeMusicStateCommand(stateGroup, stateName));
     }
 }
--- Assets/Scripts/UI/GameSettings/Volume/VolumeSettingSlider.cs
+++ Assets/Scripts/UI/GameSettings/Volume/VolumeSettingSlider.cs
@@ -2,12 +2,14 @@
 using Managers;
 using UI.Core;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.GameSettings.Volume
 {
     public class VolumeSettingSlider : DialogueComponent<SettingsDialogue>
     {
         [SerializeField] private VolumeParameter volumeParameter;
+        [SerializeField] private Slider slider;
 
         private AudioManager audioManager;
 
@@ -22,6 +24,8 @@ namespace UI.GameSettings.Volume
         protected override void OnComponentAwake()
         {
             audioManager = ManagerLocator.Get<AudioManager>();
+
+            slider.value = audioManager.GetVolume(volumeParameter.ToString());
         }
 
         #endregion
